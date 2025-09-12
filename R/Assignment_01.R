100fde3b1a43cba0b06d56046e2a63baedb03b7d
Robert Walsh <robertwalsh@usf.edu>
2025-09-12 14:54:42 -0400

    modified the assignment01.R file in the .R folder to be a function only

--- R/Assignment_01.R
+++ R/Assignment_01.R
@@ -1,11 +1,6 @@
 # This is a corrected version of the myMean() function that correctly returns
-# the mean of 'assignment2'
+# the mean of an input variable 'x'
 
-assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
-
-myMean <- function(assignment2) {
-  return(sum(assignment2) / length(assignment2))
+myMean <- function(x) {
+  return(sum(x) / length(x))
 }
-
-# Run myMean(assignment2) and record the output:
-myMean(assignment2)
