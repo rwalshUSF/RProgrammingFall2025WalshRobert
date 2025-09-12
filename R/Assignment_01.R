daf1ae69cab0f27a1842af9e578425be95043b8d
Robert Walsh <robertwalsh@usf.edu>
2025-09-12 15:18:33 -0400

    add documentation for myMean()

--- R/Assignment_01.R
+++ R/Assignment_01.R
@@ -1,6 +1,16 @@
 # This is a corrected version of the myMean() function that correctly returns
 # the mean of an input variable 'x'
 
+#' myMean()
+#'
+#' @param x A numeric vector
+#'
+#' @returns the calculated mean of the vector
+#' @export
+#'
+#' @examples
+#' x<-c(10,10,10)
+#' myMean(x)
 myMean <- function(x) {
   return(sum(x) / length(x))
 }
